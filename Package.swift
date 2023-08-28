ecc0cdbfe0b92b6b6127b22efe62863c8594bca6
Mahmudul Hasan <mhasan341@gmail.com>
2023-08-29 01:38:47 +0600

    Tests are removed for now

--- Package.swift
+++ Package.swift
@@ -21,8 +21,5 @@ let package = Package(
         .target(
             name: "SwiftBadge",
             dependencies: []),
-        .testTarget(
-            name: "SwiftBadgeTests",
-            dependencies: ["SwiftBadge"]),
     ]
 )
